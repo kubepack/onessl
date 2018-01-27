9c612bd955e08bc82b7dd6b01c695b0261bb5274
tamal <tamal@appscode.com>
2018-01-27 01:18:32 -0800

    Add get kube-ca

--- cmds/root.go
+++ cmds/root.go
@@ -39,6 +39,7 @@ func NewRootCmd(version string) *cobra.Command {
 	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 	// ref: https://github.com/kubernetes/kubernetes/issues/17162#issuecomment-225596212
 	flag.CommandLine.Parse([]string{})
+	flag.Set("stderrthreshold", "ERROR")
 
 	rootCmd.AddCommand(NewCmdCreate())
 	rootCmd.AddCommand(NewCmdGet())
