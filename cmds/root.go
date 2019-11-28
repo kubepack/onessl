4174b58a39c9417b4b076fd9224085cbebb75659
Tamal Saha <tamal@appscode.com>
2019-11-28 04:24:04 -0800

    Update client-go to kubernetes-1.16.3 (#73)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/root.go
+++ cmds/root.go
@@ -1,3 +1,19 @@
+/*
+Copyright The Kubepack Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cmds
 
 import (
@@ -7,7 +23,8 @@ import (
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	cliflag "k8s.io/component-base/cli/flag"
-	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"kmodules.xyz/client-go/logs"
 )
 
 func NewRootCmd() *cobra.Command {
@@ -28,10 +45,7 @@ func NewRootCmd() *cobra.Command {
 	matchVersionKubeConfigFlags.AddFlags(flags)
 
 	flags.AddGoFlagSet(flag.CommandLine)
-	// ref: https://github.com/kubernetes/kubernetes/issues/17162#issuecomment-225596212
-	flag.CommandLine.Parse([]string{})
-
-	flag.Set("stderrthreshold", "ERROR")
+	logs.ParseFlags()
 
 	rootCmd.AddCommand(NewCmdBase64())
 	rootCmd.AddCommand(NewCmdCreate())
